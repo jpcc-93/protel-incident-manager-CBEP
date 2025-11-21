ee23b874d5a4bc457a41b55cb3de674d3febc079
jpcc-93 <jpcc.993@gmail.com>
2025-11-21 09:06:13 -0500

    se corrige error de carga

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -10,7 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideCharts(withDefaultRegisterables()),
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch())
